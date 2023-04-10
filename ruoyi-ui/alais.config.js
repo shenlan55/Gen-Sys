967d749130ecd69b2e60a45ad25693ea614c9526
lasu <806955497@qq.com>
2023-04-10 15:07:07 +0800

    解决前端@路径不能跳转到对应资源

---
+++ ruoyi-ui/alais.config.js
@@ -0,0 +1,15 @@
+'use script'
+
+const path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  resolve: {
+    alias: {
+      '@': resolve('src')
+    }
+  }
+};
